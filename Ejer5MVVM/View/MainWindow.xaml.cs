63ca60b9ae5119f9e476c3a6202b5eb624b4474b
Juan Carlos Cárdenas García <Jccarcia98@gmail.com>
2020-04-01 18:31:20 +0200

    Estructura

--- Ejer5MVVM/View/MainWindow.xaml.cs
+++ Ejer5MVVM/View/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Ejer5MVVM.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,9 @@ namespace Ejer5MVVM
         public MainWindow()
         {
             InitializeComponent();
+
+            var conectarVM = new MainViewModel();
+            DataContext = conectarVM;
         }
     }
 }
